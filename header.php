7a6a9dd128750139a8083b2209c012b2d90a2817
Richard <r.mahr@omnicom.de>
2014-08-15 13:57:12 +0200

    Header + Menu

--- header.php
+++ header.php
@@ -87,18 +87,31 @@
 							$header_image_height = HEADER_IMAGE_HEIGHT;
 						}
 					?>
-						<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><div id="headerimg"></div></a>
+						<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+						<div id="headerimg">
+							<div class="headerinfo" id="headerinfo-r">
+							
+							
+							
+							</div>
+							<div class="headerinfo"  id="headerinfo-l">
+							
+								<div id="access" role="navigation">
+									<?php /* Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff */ ?>
+									<?php /* Our navigation menu. If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assiged to the primary position is the one used. If none is assigned, the menu with the lowest ID is used. */ ?>
+									<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
+								</div><!-- #access -->
+							
+							</div>
+						</div></a>
 					<?php endif; ?>
 			</div><!-- #branding -->
-			<div id="gigs">
-				<span><?php if ( function_exists('insert_newsticker') ) { insert_newsticker(); } ?></span>
-			</div>
-			<div id="access" role="navigation">
-			  <?php /* Allow screen readers / text browsers to skip the navigation menu and get right to the good stuff */ ?>
-				<div class="skip-link screen-reader-text"><a href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentyten' ); ?>"><?php _e( 'Skip to content', 'twentyten' ); ?></a></div>
-				<?php /* Our navigation menu. If one isn't filled out, wp_nav_menu falls back to wp_page_menu. The menu assiged to the primary position is the one used. If none is assigned, the menu with the lowest ID is used. */ ?>
-				<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'primary' ) ); ?>
-			</div><!-- #access -->
+			
+			<!--div id="gigs">
+				<span><?php /*if ( function_exists('insert_newsticker') ) { insert_newsticker(); }*/ ?></span>
+			</div-->
+			
+
 		</div><!-- #masthead -->
 	</div><!-- #header -->
 
